d27fdd25c56847e1a066829fff36fda43f4ea7dc
Mauro Moran Marcos <mauro.moranm@udc.es>
2023-12-18 18:00:09 +0100

    Añadidas sentencias SQL para añadir datos

--- src/sql/MySQLCreateTables.sql
+++ src/sql/MySQLCreateTables.sql
@@ -27,3 +27,14 @@ CREATE TABLE Incidencia ( idIncidencia INT NOT NULL AUTO_INCREMENT,
                           fechaCita DATETIME,
                           CONSTRAINT IncidenciaPK PRIMARY KEY(idIncidencia)
                         ) ENGINE = InnoDB;
+
+
+
+INSERT INTO DetallesCompra (numeroSerieProducto, email, precio, fechaCompra, nombre) VALUES ('carretilla1', 'comprador@udc.es', 10, DATE_ADD(DATE(NOW()), INTERVAL '0 00:05' DAY_MINUTE), 'carretilla');
+
+INSERT INTO DetallesCompra (numeroSerieProducto, email, precio, fechaCompra, nombre) VALUES ('123cuadro', 'elquecompra@udc.es', 20, DATE_ADD(DATE(NOW()), INTERVAL '0 17:35' DAY_MINUTE), 'cuadro');
+
+INSERT INTO DetallesCompra (numeroSerieProducto, email, precio, fechaCompra, nombre) VALUES ('tabla99planchar', 'comprador@udc.es', 7, DATE_ADD(DATE(NOW()), INTERVAL '0 12:05' DAY_MINUTE), 'tabla planchar');
+
+INSERT INTO DetallesCompra (numeroSerieProducto, email, precio, fechaCompra, nombre) VALUES ('tabla2planchar', 'yocompro@udc.es', 7, DATE_ADD(DATE(NOW()), INTERVAL '0 07:55' DAY_MINUTE), 'tabla planchar');
+
